c725377ca87d337568f9eb3d57d2768cecc5855f
森川裕子 <4432yuko@morikawayuukonoMacBook-ea.local>
2018-04-26 00:17:19 +0900

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
-  devise_for :users
   root  'static_pages#home'
+  resources :users, only: [:show]
+  devise_for :users
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
