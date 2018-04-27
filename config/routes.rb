1e969d5dacbcbad9caf133e2390a8bb3c33c87eb
森川裕子 <4432yuko@morikawayuukonoMacBook-ea.local>
2018-04-27 21:53:49 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 SampleApp::Application.routes.draw do
-  root  'static_pages#home'
+  
+
+　root  'static_pages#home'
+  
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+
+
   resources :users, only: [:show]
-  devise_for :users
+ 
+ 
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
