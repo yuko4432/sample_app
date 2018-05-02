b371b3f18366f69c2b2783426102243e3e479854
森川裕子 <4432yuko@morikawayuukonoMacBook-ea.local>
2018-05-02 13:54:16 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   validates :name, presence: true, length: { maximum: 50 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 SampleApp::Application.routes.draw do
+
 	root  'static_pages#home'
-  
-  devise_for :users, :controllers => {
-    :registrations => "registrations"
-  }
+	devise_for :users, :controllers => {
+		:registrations => "registrations"
+	}
+	resources :users, only: [:show, :index, :destroy]
+	resources :microposts, only: [:create, :destroy]
 
+	root  'static_pages#home'
 
-  resources :users, only: [:show, :index, :destroy]
- 
- 
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-  
+	match '/help', to: 'static_pages#help', via: 'get'
+	match '/about', to: 'static_pages#about', via: 'get'
+	match '/contact', to: 'static_pages#contact', via: 'get'
 end
