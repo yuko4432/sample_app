ed3dd607cd909d104ae7431c73f86ff4361c3408
森川裕子 <4432yuko@morikawayuukonoMacBook-ea.local>
2018-05-02 17:04:27 +0900

    Make a Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,28 @@
 class User < ApplicationRecord
   
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", class_name:  "Relationship", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         def feed
+  def feed
     Micropost.where("user_id = ?", id)
   end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
 
   validates :name, presence: true, length: { maximum: 50 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 SampleApp::Application.routes.draw do
-
 	root  'static_pages#home'
 	devise_for :users, :controllers => {
 		:registrations => "registrations"
 	}
 	resources :users, only: [:show, :index, :destroy]
 	resources :microposts, only: [:create, :destroy]
-
 	root  'static_pages#home'
-
 	match '/help', to: 'static_pages#help', via: 'get'
 	match '/about', to: 'static_pages#about', via: 'get'
 	match '/contact', to: 'static_pages#contact', via: 'get'
