fb784e786b96c5fb2673b87e64bdd2f8fc66edc1
Rybot <tokorotenpi2bagu@gmail.com>
2018-05-02 22:36:57 -0700

    Userモデル内の命名でfollowedsをfollowed_usersに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :relationships, foreign_key: "follower_id", class_name:  "Relationship", dependent: :destroy
   has_many :relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent: :destroy
   has_many :followers, through: :relationships, source: :follower
-  has_many :followeds, through: :relationships, source: :followed
+  has_many :followed_users, through: :relationships, source: :followed
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
